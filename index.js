c641b4921b6a035d995ed455e48c694542f4ca40
SagorikaKubi <sagorika.tech@gmail.com>
2021-04-19 17:51:21 +0600

    add

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const app = express()
+
 const MongoClient = require('mongodb').MongoClient;
 const ObjectID = require("mongodb").ObjectID;
 const cors = require("cors");
@@ -7,9 +7,9 @@ const bodyParser = require("body-parser");
 require("dotenv").config()
 
 const port = process.env.PORT || 5500
-
+const app = express()
 app.use(cors());
-app.use(bodyParser());
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -23,11 +23,11 @@ client.connect(err => {
     const servicesCollection = client.db("weddingPhotography").collection("services");
     const reviewsCollection = client.db("weddingPhotography").collection("reviews");
     const bookingsCollection = client.db("weddingPhotography").collection("bookings");
+    const adminCollection = client.db("weddingPhotography").collection("admin");
     console.log("database Connected")
 
     app.post("/addService", (req, res) => {
         const newService = req.body;
-
         servicesCollection.insertOne(newService)
             .then(result => {
                 console.log("inserted count", result)
@@ -47,6 +47,12 @@ client.connect(err => {
         servicesCollection.find({ _id: ObjectID(req.params.id) })
             .toArray((err, items) => {
                 res.send(items[0]);
+            })
+    })
+
+    app.delete("/delete/:id", (req, res) => {
+        servicesCollection.deleteOne({ _id: ObjectID(req.params.id) })
+            .then(result => {
             })
     })
 
@@ -78,14 +84,37 @@ client.connect(err => {
             })
     })
 
+
+
+
+
     app.get("/bookings", (req, res) => {
         console.log(req.query.email);
         let email = req.query.email;
-        bookingsCollection.find({ email: email })
-            .toArray((err, items) => {
-                res.send(items)
+        adminCollection.find({ email: email })
+            .toArray((err, admin) => {
+                let filter = {};
+                if (admin.length === 0) {
+                    filter.email = email
+                }
+                bookingsCollection.find(filter)
+                    .toArray((err, items) => {
+                        res.send(items)
+                    })
             })
     })
+
+
+
+
+    // app.get("/bookings", (req, res) => {
+    //     console.log(req.query.email);
+    //     let email = req.query.email;
+    //     bookingsCollection.find({ email: email })
+    //         .toArray((err, items) => {
+    //             res.send(items)
+    //         })
+    // })
 
     // app.get("/bookings", (req, res) => {
     //     bookingsCollection.find({})
@@ -94,11 +123,27 @@ client.connect(err => {
     //         })
     // })
 
-    app.delete("/delete/:id", (req, res) => {
-        servicesCollection.deleteOne({ _id: ObjectID(req.params.id) })
+
+    app.post("/makeAdmin", (req, res) => {
+        const newAdmin = req.body;
+        adminCollection.insertOne(newAdmin)
             .then(result => {
+                console.log("inserted count", result)
+                res.send(result.insertedCount > 0)
             })
     })
+
+    app.post('/isAdmins', (req, res) => {
+        console.log(req.body.email);
+        const email = req.body.email;
+
+        adminCollection.find({ email: email })
+            .toArray((err, admin) => {
+                res.send(admin.length > 0)
+                console.log(admin.length > 0)
+            })
+    })
+
 
 });
 
